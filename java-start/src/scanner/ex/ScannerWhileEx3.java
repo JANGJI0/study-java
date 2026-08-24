d6ee87cc3de4faed9693890bf4f8e5a727931ae9
JANGJI0 <qlvm2002@gmail.com>
2026-08-24 19:35:30 +0900

    [20260824] scanner 문제
    - while문, if문을 적용하여 문제 풀기
    - nextLine() 이후에 줄바꿈 오류가 생기므로 빈 scanner.nextLine(); 사용해야한다.

---
+++ java-start/src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,35 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+        int input = 0;
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
+        while (true) {
+            input = scanner.nextInt();
+
+            if (input == -1) {
+                break;
+            }
+            sum += input;
+            count++;
+        }
+        /*
+         * while((input = scanner.nextInt()) != -1){
+         *   sum += input
+         *   count++
+         * }    축약할 수 있다.
+         * */
+
+        double average = (double)sum / count;
+        System.out.println("입력한 숫자들의 합게: " + sum);
+        System.out.println("입력한 숫자들의 합게: " + average);
+    }
+}
