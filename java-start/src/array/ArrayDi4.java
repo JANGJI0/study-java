f5337b28e58f7c32abb2eaec1af48fb105129e7e
JANGJI0 <qlvm2002@gmail.com>
2026-08-25 20:45:48 +0900

    [20260825]  배열
    - 배열
    - 자바는 배열 생성할때 내부값을 자동으로 초기화
    - 기본형은 선언과 동시에 사이즈가 정적으로 정해지지만, 참조형은 동적으로 크기가 변해서 유연성을 제공
    - 2차원 배열
    - 리펙토링: 기존의 코드기능은 유지하면서 내부 구조를 개선하여 가독성을 높이고, 유지보수를 용이하게 하는 과정
    - 향샹된 for문: foreach
        못하는경우: 증가하는 i와 같은 인덱스 값을 직접 사용할때
    - foreach문 자동완성 단축키: iter

---
+++ java-start/src/array/ArrayDi4.java
@@ -0,0 +1,23 @@
+package array;
+
+public class ArrayDi4 {
+
+    public static void main(String[] args) {
+        // 2x3 2차원 배열
+        int[][] arr = new int[5][5];
+
+        int i = 1;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = i++;
+            }
+        }
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " "); // 0열 출력
+            }
+            System.out.println(); //한 행이 끝나면 라인을 변경한다.
+        }
+    }
+}
